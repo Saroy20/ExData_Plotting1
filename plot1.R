a8d21d9a896410a931d8ba49d42cfae6c08e0e73
Sandra Roy <girlook.17@gmail.com>
2021-09-13 11:29:03 -0700

    fixing size of figure

--- plot1.R
+++ plot1.R
@@ -7,5 +7,5 @@ data$Date<-as.Date(data$Date,"%d/%m/%Y")
 subsetdata<-subset(data,Date=="2007-02-01"|Date=="2007-02-02")
 hist(subsetdata$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 
-dev.copy(png,file="plot1.png")
+dev.copy(png,file="figure/plot1.png",width=480,height=480)
 dev.off()
