256de8bfc9cfa6105a2767dc02b8ddbdb4a34b96
Sandra Roy <girlook.17@gmail.com>
2021-09-13 11:29:26 -0700

    update

--- plot2.R
+++ plot2.R
@@ -3,9 +3,10 @@ library(dplyr)
 
 data<-read.table("household_power_consumption.txt",stringsAsFactor = FALSE,sep=";",header=TRUE,na.strings = "?")
 data$Date<-as.Date(data$Date,"%d/%m/%Y")
-
 subsetdata<-subset(data,Date=="2007-02-01"|Date=="2007-02-02")
-hist(subsetdata$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+subsetdata$datetime<-(as.POSIXct(paste(subsetdata$Date, subsetdata$Time), format="%Y-%m-%d %H:%M:%S"))
 
-dev.copy(png,file="figure/plot1.png")
+plot(subsetdata$datetime,subsetdata$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+
+dev.copy(png,file="figure/plot2.png",width=480,height=480)
 dev.off()
